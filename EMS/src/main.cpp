9719b86dde2b65d97c512846f85e4dc699a79672
Jordan-Robles <jordan.james.robles@gmail.com>
2026-04-12 20:27:55 +1000

    v1 self test routine

--- EMS/src/main.cpp
+++ EMS/src/main.cpp
@@ -11,15 +11,27 @@ const int y = A1;
 const int z = A2;
 const int stPin = 13;
 
+float xAxis = 0;
+float yAxis = 0;
+float zAxis = 0;
+
+// Self test stuff
+int selfTestCount[] = {0, 0, 0}; // used to keep track of seft test pins
+int selftTestState = 0; //used to iterate through each axis test state for now till i develop a function to handle whole sub-Routine
+float xArray[];
+int sampleIndex = 0;
+const unsigned long selfTestInterval = 1000; //gives us 3 seconds to read the axis value and determine if the sensor is working
+
+
 //Setting input pins for the buttons
-const int button1 = 2;
-const int button2 = 3;
-const int button3 = 4;
+const int button1 = 2; // Mode = toggles between each sub routine 
+const int button2 = 3; // Next = insdie each subroutine we can flicker between different data to be displayed
+const int button3 = 4; //Action button = COnfirms mode selection, start and stop
 
 //Setting pins for LED
-const int led1 = 5;
+const int led1 = 7;
 const int led2 = 6;
-const int led3 = 7;
+const int led3 = 5;
 
 //Setting pins for the GC9A01
 const int DIN = A4;
@@ -27,11 +39,15 @@ const int CLK = A5;
 const int CS = 10;
 const int DC = 9;
 
+//Time
+unsigned long currentTime =0;
+unsigned long previousTime =0;
 
-
-//Set input pins for the ADXL to the Arduino nano
 int currentCase = 0; // Set case = 0 to initialise system to starting profile
-int selfTestCount[] = {0, 0, 0}; // used to keep track of seft test pins
+
+
+
+
 void setup() {
   // Set pinMode for x, y, z pins as INPUT
   pinMode(x, INPUT);
@@ -39,50 +55,94 @@ void setup() {
   pinMode(z,INPUT);
   pinMode(stPin,OUTPUT);
 
+  //Set buttons as INPUT
+  pinMode(button1, INPUT);
+  pinMode(button2,INPUT);
+  pinMode(button3,INPUT);
+
+  //Set State Led
+  pinMode(led1, OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+
   Serial.begin(115200); //have it set as 112500 as of the moment to use with my existing BMI160 Accelerometer jsut to test code
 
 }
+
+
+float averageArray(float array[], int size){
+  long sum = 0;
+  for(int i = 0; i <size; i++){
+    sum = sum + array[i];
+  }
+  float average = sum / size;
+
+  return average;
+}
+
+int selfTest(const int axis, int testCount){
+  currentTime = millis();
+  if(currentTime - previousTime >= selfTestInterval && sampleIndex < 5){ //takes a sample every 1 second till a total of 5 samples is taken
+    float reading = analogRead(axis);
+    xArray[sampleIndex] = reading;
+    sampleIndex += 1;
+    previousTime = currentTime;
+  }
+
+  float averageReading = averageArray(xArray, 5);
+
+  if(averageReading > 1 && averageReading < 1.5){// x in range of expected x value
+
+    Serial.println("axis Good");
+    selfTestCount[testCount] = 1;
+  }
+  else{
+    Serial.println("axis no Good");
+  } 
+  return selftTestState += 1;
+}
+
+
+
 void loop() {
   switch(currentCase){
-    case 0:
+    case 0: // testing BMI160
 
-    if //Self test button pressed == HIGH
-      currentCase = 1; // Test X
+    if(digitalRead(button1) == HIGH){//Self test button pressed == HIGH
+      Serial.println("Self Test initiaed");
+      previousTime = millis();
+      currentCase = 1; // Test X 
       //Print to Screen
+    }
     else // Continue with step tracking routine 
     break;
 
+    case 1: // Self test Routine
     
+      if(selftTestState == 0){ // xAxis
+        Serial.println("Place Device with arrow pointing to * and press action button to continue");
+        if(digitalRead(button3) == HIGH){
+          Serial.println("Starting...");
+        }
+        selfTest(xAxis, selftTestState);
+      }
 
-    case 1:
-     //anologRead(x)
-     if // x in == expected x value
-      //print to screen "X axis Good"
-      selfTestCount[0] = 1;
-      case = 2; // Test Y
-      else if //x in != expected x value
-        //print to screen "X axis no good"
-        currentCase = 2; // Test Y
-    break;
+      else if(selftTestState == 1){ // yAxis
+        Serial.println("Place Device with arrow pointing to * and press action button to continue");
+        if(digitalRead(button3) == HIGH){
+          Serial.println("Starting...");
+        }
+        selfTest(yAxis, selftTestState);
+      }
 
-    case 2:
-      //anologRead(x)
-     if // y in == expected y value
-      //print to screen "Y axis Good"
-      selfTestCount[1] = 1;
-      currentCase = 3; // Test Z
-      else if //y in != expected y value
-        //print to screen "y axis no good"
-    break;
+      else if(selftTestState == 2){ // zAxis
+        Serial.println("Place Device with arrow pointing to * and press action button to continue");
+        if(digitalRead(button3) == HIGH){
+          Serial.println("Starting...");
+        }        
+        selfTest(yAxis, selftTestState);
+      }
 
-    case 3:
-      //anologRead(x)
-     if // z in == expected z value
-      //print to screen "Z axis Good"
-      selfTestCount[2] = 1;
-      currentCase = 1; // Go back to Case 0 to resume step tracking routine
-      else if //z in != expected zvalue
-        //print to screen "Z axis no good"
     break;
   }
 }
